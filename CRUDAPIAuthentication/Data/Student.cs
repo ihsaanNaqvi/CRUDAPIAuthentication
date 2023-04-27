88bd0c7310af66b9ec3f4fa88b05c56e61924f6c
ihsaan.abbas <ihsaan.abbas@SID-IHSAAN-ABBA>
2023-04-27 10:06:43 +0500

    Add project files.

---
+++ CRUDAPIAuthentication/Data/Student.cs
@@ -0,0 +1,12 @@
+﻿namespace CRUDAPIAuthentication.Data
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string Name { get; set; }
+        public string Roll { get; set; }
+    }
+}
+
+
+ 
